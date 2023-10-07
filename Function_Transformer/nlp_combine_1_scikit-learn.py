954bf5463bc9bc9f696db10a04642e03a3c7cc1b
jtquisenberry <20849606+jtquisenberry@users.noreply.github.com>
2023-10-06 20:35:00 -0500

    Create nlp_combine_1_scikit-learn.py

---
+++ Function_Transformer/nlp_combine_1_scikit-learn.py
@@ -0,0 +1,51 @@
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
+
+# Create Function Transformer to use Feature Union
+def get_numeric_data(x):
+    return [record[:-2].astype(float) for record in x]
+
+def get_text_data(x):
+    return [record[-1] for record in x]
+
+transfomer_numeric = FunctionTransformer(get_numeric_data)
+transformer_text = FunctionTransformer(get_text_data)
+
+# Create a pipeline to concatenate Tfidf Vector and Numeric data
+# Use RandomForestClassifier as an example
+pipeline = Pipeline([
+    ('features', FeatureUnion([
+            ('numeric_features', Pipeline([
+                ('selector', transfomer_numeric)
+            ])),
+             ('text_features', Pipeline([
+                ('selector', transformer_text),
+                ('vec', TfidfVectorizer(analyzer='word'))
+            ]))
+         ])),
+    ('clf', RandomForestClassifier())
+])
+
+# Grid Search Parameters for RandomForest
+param_grid = {'clf__n_estimators': np.linspace(1, 100, 10, dtype=int),
+              'clf__min_samples_split': [3, 10],
+              'clf__min_samples_leaf': [3],
+              'clf__max_features': [7],
+              'clf__max_depth': [None],
+              'clf__criterion': ['gini'],
+              'clf__bootstrap': [False]}
+
+# Training config
+kfold = StratifiedKFold(n_splits=7)
+scoring = {'Accuracy': 'accuracy', 'F1': 'f1_macro'}
+refit = 'F1'
+
+# Perform GridSearch
+rf_model = GridSearchCV(pipeline, param_grid=param_grid, cv=kfold, scoring=scoring, 
+                         refit=refit, n_jobs=-1, return_train_score=True, verbose=1)
+rf_model.fit(X_train, Y_train)
+rf_best = rf_model.best_estimator_
